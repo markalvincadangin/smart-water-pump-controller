49b6887b08c7dbecbb2a7276d3b7b01751ce2950
Mark Alvin Cadangin <markcadangin@gmail.com>
2026-07-27 16:51:18 +0800

    fix(app): move navigation-compose dependency inside dependencies block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,7 @@ dependencies {
     implementation("com.polidea.rxandroidble3:rxandroidble:3.0.1")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
-}
+
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+}
