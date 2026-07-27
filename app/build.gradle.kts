ebf62b4ff6628819ce4fcb200900727fdcaf7e3f
Mark Alvin Cadangin <markcadangin@gmail.com>
2026-07-27 17:23:39 +0800

    fix(app): update RxAndroidBle version to 3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database")
     
     // BLE (RxAndroidBle)
-    implementation("com.polidea.rxandroidble3:rxandroidble:3.0.1")
+    implementation("com.polidea.rxandroidble3:rxandroidble:3.0.0")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 
